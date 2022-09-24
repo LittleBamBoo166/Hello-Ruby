b323a2441a82d24ea0549856f3463a86bc624485
Phi Thuong <chontennguoidungcuaban166@gmail.com>
2022-09-24 17:16:39 +0700

    bai tap co ban: bai 3

---
+++ bai-tap-co-ban-sep-17/bai-3.rb
@@ -0,0 +1,7 @@
+# Viết chương trình để nhắc người dùng nhập tên của họ, sau đó in kết quả trên màn hình.
+while name = ""
+    puts 'Nhap ten: '
+    name = gets
+    break
+end
+puts "Ten: #{name}"
