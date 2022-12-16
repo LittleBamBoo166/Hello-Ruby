32c9795c4b24b3cc49de7ed3941466733eb74012
Phi Thuong <chontennguoidungcuaban166@gmail.com>
2022-12-17 01:16:27 +0700

    final

--- bai-tap-oop-oct-12/bai-3/Admission.rb
+++ bai-tap-oop-oct-12/bai-3/Admission.rb
@@ -8,12 +8,65 @@ class Admission.rb
     def addACandidate
         print 'a candidate id: '
         id = gets
+        print 'full name: '
+        name = gets
+        print 'address: '
+        address = gets
+        print 'priority: '
+        priority = gets
         print 'math score: '
         math = gets
         print 'physics score: '
         physics = gets
         print 'chemistry score: '
         chemistry = gets
-        
+        candidate = ACandidate.new(id, name, address, priority,math, physics, chemistry)
+        @@candidateList.push(candidate)
+    end
+
+    def addBCandidate
+        print 'b candidate id: '
+        id = gets
+        print 'full name: '
+        name = gets
+        print 'address: '
+        address = gets
+        print 'priority: '
+        priority = gets
+        print 'math score: '
+        math = gets
+        print 'chemistry score: '
+        chemistry = gets
+        print 'biology score: '
+        biology = gets
+        candidate = BCandidate.new(id, name, address, priority, math, chemistry, biology)
+        @@candidateList.push(candidate)
+    end
+
+    def addCCandidate
+        print 'c candidate id: '
+        id = gets
+        print 'full name: '
+        name = gets
+        print 'address: '
+        address = gets
+        print 'priority: '
+        priority = gets
+        print 'literature score: '
+        literature = gets
+        print 'history score: '
+        history = gets
+        print 'geography score: '
+        geography = gets
+        candidate = CCandidate.new(id, name, adress, priority, literature, history, geography)
+        @@candidateList.push(candidate)
+    end
+
+    def showCandidateInfo
+        length = @@candidateList.lenght
+        for i in 0..length do
+            @@candidateList[i].display
+            puts '-----------------------------'
+        end
     end
 end
